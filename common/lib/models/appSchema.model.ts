f4064faab74ec95b0298b64c4c8441ea24fa7acd
Arthur <art7cf@protonmail.com>
2024-08-25 19:40:39 +0300

    feat: Update full app schema

--- common/lib/models/appSchema.model.ts
+++ common/lib/models/appSchema.model.ts
@@ -14,9 +14,19 @@ export interface FullAppSchema extends AppSchema {
   dataSources: PublicDataSource[];
   procedures: Procedure[];
   usingTsc: boolean;
-  
   sandbox: {
     // Only available in development mode
     developmentServerUrl?: string;
   };
+
+  // Only available in development mode
+  devSyncServerUrl?: string;
+  os?: {
+    platform: string;
+    type: string;
+    release: string;
+  };
+  absoluteDirPath?: string;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
 }

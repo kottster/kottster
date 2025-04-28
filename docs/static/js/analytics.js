7ec8e6e39b3bfd5f0f4471008911e1325e55836b
Arthur <art7cf@protonmail.com>
2025-04-28 18:25:46 +0300

    docs: Update analytics script

--- docs/static/js/analytics.js
+++ docs/static/js/analytics.js
@@ -7,3 +7,15 @@
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
   a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+// Plausible
+(function () {
+  var script = document.createElement('script');
+
+  script.setAttribute('defer', '');
+  script.setAttribute('data-domain', 'kottster.app');
+  script.setAttribute('data-api', 'https://items-catalog.kottster.app/by-category/event');
+  script.setAttribute('src', 'https://items-catalog.kottster.app/by-category/script.js');
+
+  document.head.appendChild(script);
+})();

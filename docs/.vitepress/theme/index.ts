5646d00c58bce4a3b27cca39f7bccf876c93c6f0
Arthur <art7cf@protonmail.com>
2025-08-20 14:49:09 +0300

    docs: Add mixpanel script

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,21 @@
-import Theme from 'vitepress/theme'
-import 'virtual:group-icons.css'
+import DefaultTheme from 'vitepress/theme';
+import mixpanel from 'mixpanel-browser';
 
-export default Theme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app, router }) {
+    if (typeof window !== 'undefined') {
+      console.info('mixpanel.init');
+      mixpanel.init("fc5e794d2cfef6c0fd69f67eaae370d8", {
+        api_host: 'https://reviews-catalog.kottster.app',
+        debug: false,
+        track_pageview: true,
+        persistence: 'localStorage',
+        
+        cookie_domain: '.kottster.app',
+        cross_subdomain_cookie: true,
+        ignore_dnt: true,
+      });
+    }
+  }
+};
